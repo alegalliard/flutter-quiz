0f9833265156efcf2c81bd024fc0ad7681b410c2
Alessandra Souza <ale.sz.design@gmail.com>
2020-08-04 22:33:47 -0300

    using widget. to get properties of class

--- lib/finance/widgets/new_transaction.dart
+++ lib/finance/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   void submitData() {
     final enteredTitle = titleController.text;
@@ -15,7 +22,9 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    addTx(enteredTitle, enteredAmount);
+    //widget. permite acessar as propriedades e métodos do widget dentro do state
+    widget.addTx(enteredTitle, enteredAmount);
+    
   }
   
   @override
